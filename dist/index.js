1b7d92ede922df402fdf58a21122c799fb375f45
Sebastian Tschöpel <sebastian.tschoepel@leanix.net>
2020-06-18 16:45:05 +0200

    Add wait for completion

--- dist/index.js
+++ dist/index.js
@@ -123,8 +123,9 @@ const axios = __webpack_require__(900)
 // External input
 const username = process.env.JENKINS_USER
 const apitoken = process.env.JENKINS_TOKEN
-const parameters = `${core.getInput('parameters')}`
-var jobBuildUrl = `${core.getInput('jobBuildUrl')}`
+const parameters = `${core.getInput("parameters")}`
+const waitForCompletion = `${core.getInput("waitForCompletion")}`
+var jobBuildUrl = `${core.getInput("jobBuildUrl")}`
 
 const poll = async ({ fn, validate, interval, maxAttempts }) => {
     let attempts = 0
@@ -134,7 +135,7 @@ const poll = async ({ fn, validate, interval, maxAttempts }) => {
         if (validate(result)) {
             return resolve(result)
         } else if (maxAttempts && attempts === maxAttempts) {
-            return reject(new Error('Exceeded max attempts'))
+            return reject(new Error("Exceeded max attempts"))
         } else {
             setTimeout(executePoll, interval, resolve, reject)
         }
@@ -142,27 +143,30 @@ const poll = async ({ fn, validate, interval, maxAttempts }) => {
     return new Promise(executePoll)
 }
 
-function pollBuildStatus(queuedItemUrl) {
-    core.info(`Polling status of job via ${queuedItemUrl}`)
-    poll({
+const pollForBuildStart = async (queuedItemUrl) => {
+    return poll({
         fn: async () => {
             return axios.post(queuedItemUrl, {}, { auth: basicAuth })
-          },
+        },
         validate: response => response.data.executable !== undefined,
         interval: 1000,
         maxAttempts: 60
     })
-        .then((response) => {
-            core.info("Job successfully started.")
-            core.info(`Build URL is ${response.data.executable.url}`)
-        })
-        .catch((error) => {
-            core.setFailed(error.message)
-        })
+}
+
+const pollForBuildCompletion = async (buildUrl) => {
+    return poll({
+        fn: async () => {
+            return axios.post(buildUrl, {}, { auth: basicAuth })
+        },
+        validate: response => response.data.result !== null,
+        interval: 1000,
+        maxAttempts: 60 * 30
+    })
 }
 
 if (parameters !== undefined && parameters !== "") {
-    const queryParam = parameters.trim().replace(/\n+/, "&")
+    const queryParam = parameters.trim().replace(/\n+/g, "&")
     core.info(`Using query params: ${queryParam}`)
     jobBuildUrl = `${jobBuildUrl}?${queryParam}`
 }
@@ -174,9 +178,32 @@ const basicAuth = {
 core.info(`Triggering job: ${jobBuildUrl}`)
 axios.post(jobBuildUrl, {}, { auth: basicAuth })
     .then((response) => {
-        core.info("Job triggered successfully.")
-        const queueItem = `${response.headers.location}api/json`
-        pollBuildStatus(queueItem)
+        core.info("Job triggered successfully")
+        const queuedItemUrl = `${response.headers.location}api/json`
+        core.info(`Polling startup of job via ${queuedItemUrl}`)
+        pollForBuildStart(queuedItemUrl)
+            .then((response) => {
+                core.info("Job successfully started")
+                core.info(`Build URL is ${response.data.executable.url}`)
+                if (waitForCompletion == "true") {
+                    core.info("Waiting for job completion")
+                    const buildUrl = `${response.data.executable.url}api/json`
+                    pollForBuildCompletion(buildUrl)
+                        .then((response) => {
+                            core.info(`Job finished with result ${response.data.result}`)
+                            if (response.data.result != "SUCCESS") {
+                                core.setFailed("Unsuccessful job state")
+                            }
+                        })
+                        .catch((error) => {
+                            core.setFailed(error.message)
+                        })
+                }
+
+            })
+            .catch((error) => {
+                core.setFailed(error.message)
+            })
     })
     .catch((error) => {
         core.setFailed(error.message)
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const core = require('@actions/core')
-const axios = require('axios')
+const core = require("@actions/core")
+const axios = require("axios")
 
 // External input
 const username = process.env.JENKINS_USER
 const apitoken = process.env.JENKINS_TOKEN
-const parameters = `${core.getInput('parameters')}`
-var jobBuildUrl = `${core.getInput('jobBuildUrl')}`
+const parameters = `${core.getInput("parameters")}`
+const waitForCompletion = `${core.getInput("waitForCompletion")}`
+var jobBuildUrl = `${core.getInput("jobBuildUrl")}`
 
 const poll = async ({ fn, validate, interval, maxAttempts }) => {
     let attempts = 0
@@ -15,7 +16,7 @@ const poll = async ({ fn, validate, interval, maxAttempts }) => {
         if (validate(result)) {
             return resolve(result)
         } else if (maxAttempts && attempts === maxAttempts) {
-            return reject(new Error('Exceeded max attempts'))
+            return reject(new Error("Exceeded max attempts"))
         } else {
             setTimeout(executePoll, interval, resolve, reject)
         }
@@ -23,27 +24,30 @@ const poll = async ({ fn, validate, interval, maxAttempts }) => {
     return new Promise(executePoll)
 }
 
-function pollBuildStatus(queuedItemUrl) {
-    core.info(`Polling status of job via ${queuedItemUrl}`)
-    poll({
+const pollForBuildStart = async (queuedItemUrl) => {
+    return poll({
         fn: async () => {
             return axios.post(queuedItemUrl, {}, { auth: basicAuth })
-          },
+        },
         validate: response => response.data.executable !== undefined,
         interval: 1000,
         maxAttempts: 60
     })
-        .then((response) => {
-            core.info("Job successfully started.")
-            core.info(`Build URL is ${response.data.executable.url}`)
-        })
-        .catch((error) => {
-            core.setFailed(error.message)
-        })
+}
+
+const pollForBuildCompletion = async (buildUrl) => {
+    return poll({
+        fn: async () => {
+            return axios.post(buildUrl, {}, { auth: basicAuth })
+        },
+        validate: response => response.data.result !== null,
+        interval: 1000,
+        maxAttempts: 60 * 30
+    })
 }
 
 if (parameters !== undefined && parameters !== "") {
-    const queryParam = parameters.trim().replace(/\n+/, "&")
+    const queryParam = parameters.trim().replace(/\n+/g, "&")
     core.info(`Using query params: ${queryParam}`)
     jobBuildUrl = `${jobBuildUrl}?${queryParam}`
 }
@@ -55,9 +59,32 @@ const basicAuth = {
 core.info(`Triggering job: ${jobBuildUrl}`)
 axios.post(jobBuildUrl, {}, { auth: basicAuth })
     .then((response) => {
-        core.info("Job triggered successfully.")
-        const queueItem = `${response.headers.location}api/json`
-        pollBuildStatus(queueItem)
+        core.info("Job triggered successfully")
+        const queuedItemUrl = `${response.headers.location}api/json`
+        core.info(`Polling startup of job via ${queuedItemUrl}`)
+        pollForBuildStart(queuedItemUrl)
+            .then((response) => {
+                core.info("Job successfully started")
+                core.info(`Build URL is ${response.data.executable.url}`)
+                if (waitForCompletion == "true") {
+                    core.info("Waiting for job completion")
+                    const buildUrl = `${response.data.executable.url}api/json`
+                    pollForBuildCompletion(buildUrl)
+                        .then((response) => {
+                            core.info(`Job finished with result ${response.data.result}`)
+                            if (response.data.result != "SUCCESS") {
+                                core.setFailed("Unsuccessful job state")
+                            }
+                        })
+                        .catch((error) => {
+                            core.setFailed(error.message)
+                        })
+                }
+
+            })
+            .catch((error) => {
+                core.setFailed(error.message)
+            })
     })
     .catch((error) => {
         core.setFailed(error.message)
