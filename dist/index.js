b827eb124a4d7f966368622fd3dbedbab97a9e5a
Sebastian Tschöpel <sebastian.tschoepel@leanix.net>
2020-06-18 15:28:06 +0200

    Support query parameters

--- dist/index.js
+++ dist/index.js
@@ -117,39 +117,41 @@ module.exports = CancelToken;
 /***/ 34:
 /***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
-const core = __webpack_require__(310);
+const core = __webpack_require__(310)
 const axios = __webpack_require__(900)
 
 // External input
 const username = process.env.JENKINS_USER
 const apitoken = process.env.JENKINS_TOKEN
-const jobUrl = `${core.getInput('jobUrl')}`;
-const parameters = `${core.getInput('parameters')}`;
-core.info(parameters);
+const jobUrl = `${core.getInput('jobUrl')}`
+const parameters = `${core.getInput('parameters')}`
 
 // Prepare call
-const jobUrlBuildTrigger = `${jobUrl.replace(/\/$/, "")}/buildWithParameters`
+var jobUrlBuildTrigger = `${jobUrl.replace(/\/$/, "")}/buildWithParameters`
+if ( parameters !== "" ) {
+    jobUrlBuildTrigger = `${jobUrlBuildTrigger}?${parameters.replace("\n", "&")}`
+}
 const basicAuth = {
     username: username,
     password: apitoken
 }
 
 // Execute
-core.info(`Triggering job: ${jobUrlBuildTrigger}`);
+core.info(`Triggering job: ${jobUrlBuildTrigger}`)
 axios.post(jobUrlBuildTrigger, {}, { auth: basicAuth })
     .then((res) => {
         core.info("Job triggered successfully.")
-        const queueItem = `${res.headers.location}/api/json`;
+        const queueItem = `${res.headers.location}/api/json`
         axios.post(queueItem, {}, { auth: basicAuth })
             .then((res) => {
-                core.info(JSON.stringify(res.data, null, 1));
+                core.info(JSON.stringify(res.data, null, 1))
             })
             .catch((error) => {
-                core.setFailed(error.message);
+                core.setFailed(error.message)
             })
     })
     .catch((error) => {
-        core.setFailed(error.message);
+        core.setFailed(error.message)
     })
 
 
--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const core = require('@actions/core');
+const core = require('@actions/core')
 const axios = require('axios')
 
 // External input
 const username = process.env.JENKINS_USER
 const apitoken = process.env.JENKINS_TOKEN
-const jobUrl = `${core.getInput('jobUrl')}`;
-const parameters = `${core.getInput('parameters')}`;
-core.info(parameters);
+const jobUrl = `${core.getInput('jobUrl')}`
+const parameters = `${core.getInput('parameters')}`
 
 // Prepare call
-const jobUrlBuildTrigger = `${jobUrl.replace(/\/$/, "")}/buildWithParameters`
+var jobUrlBuildTrigger = `${jobUrl.replace(/\/$/, "")}/buildWithParameters`
+if ( parameters !== "" ) {
+    jobUrlBuildTrigger = `${jobUrlBuildTrigger}?${parameters.replace("\n", "&")}`
+}
 const basicAuth = {
     username: username,
     password: apitoken
 }
 
 // Execute
-core.info(`Triggering job: ${jobUrlBuildTrigger}`);
+core.info(`Triggering job: ${jobUrlBuildTrigger}`)
 axios.post(jobUrlBuildTrigger, {}, { auth: basicAuth })
     .then((res) => {
         core.info("Job triggered successfully.")
-        const queueItem = `${res.headers.location}/api/json`;
+        const queueItem = `${res.headers.location}/api/json`
         axios.post(queueItem, {}, { auth: basicAuth })
             .then((res) => {
-                core.info(JSON.stringify(res.data, null, 1));
+                core.info(JSON.stringify(res.data, null, 1))
             })
             .catch((error) => {
-                core.setFailed(error.message);
+                core.setFailed(error.message)
             })
     })
     .catch((error) => {
-        core.setFailed(error.message);
+        core.setFailed(error.message)
     })
